39d997325dd981c319030d495db904a18386e0fc
Shreyash Parajuli <Shreyashparajuli@outlook.com>
2022-11-09 12:00:43 +0545

    day1 with rock paper scissors

--- Practice/rock paper siccors.py
+++ Practice/rock paper siccors.py
@@ -1,15 +1,19 @@
 from random import randint as c
+from rocknal import cal as p
 cal=int(input("Rock,1/Paper,2/Scissors,3: "))
 v=c(1,3)
+user=p(cal)
+comp=p(v)
+print("you choose %s and computer choose %s" %(user, comp))
 if cal!=v:
     if cal==1 or v==1 and cal!=2:
         if cal<v and (v!=2 or cal!=2):
-            print("you win",cal,v)
+            print("you win")
         else:
-            print("you lose",cal,v)
+            print("you lose")
     elif cal>v:
-        print("you win",cal,v)
+        print("you win")
     else:
-        print("you lose",cal,v)
+        print("you lose")
 else:
-    print("Draw",cal,v)
+    print("Draw")
---
+++ Practice/rocknal.py
@@ -0,0 +1,9 @@
+def cal(a):
+    if a==1:
+        return "rock"
+    elif a==2:
+        return "paper"
+    elif a==3:
+        return "scissor"
+    else:
+        return "invalid"
