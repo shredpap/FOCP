ab6e18e5f25b4dc9cb696def53a4334c60165704
Shreyash Parajuli <Shreyashparajuli@outlook.com>
2022-11-13 22:07:06 +0545

    Function exercises

---
+++ Practice/assign.py
@@ -0,0 +1,9 @@
+def find_min(a,b):
+    if a>b:
+        return b
+    else:
+        return a 
+print(find_min(4,5))
+'''
+it defines a function called print_header(msg). returns the value provided by the ‘msg’ parameter to the screen. (prefixed by five asterisk ‘*****’) characters.
+'''
